a2c008b63141a471ff356574060218377721b50c
pawel-w88 <rws.pawlak@gmail.com>
2024-01-08 14:25:11 +0100

    fix gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/lotto-number-generator',
   plugins: [react()],
+  base: '/lotto-number-generator/',
 })
